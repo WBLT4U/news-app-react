f54c0542b95020e8f29f53bd41de8db48dc2307b
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2024-08-31 10:43:20 -0700

    a

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,24 @@ import { Link } from 'react-router-dom';
 import './navbar.css'; // Ensure you have a CSS file for styles
 
 function Nav() {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
-
     return (
-        <nav className="navbar">
-            <button className="menu-icon" onClick={toggleMenu}>
-                <i className="fas fa-bars"></i>
-            </button>
-            <ul className={`navMenu ${isOpen ? 'showMenu' : ''}`}>
-                <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
-                <li><Link to="/aboutus" onClick={toggleMenu}>About Us</Link></li>
-                <li><Link to="/contactus" onClick={toggleMenu}>Contact Us</Link></li>
-                <li><Link to="/pagination" onClick={toggleMenu}>News</Link></li>
+      <nav id="navigation">
+        {/* container */}
+        <div className="container">
+          {/* responsive-nav */}
+          <div id="responsive-nav">
+            {/* NAV */}
+            <ul className="main-nav nav navbar-nav">
+              <li className="active"><Link to="/">Home</Link></li>
+              <li><Link to="/aboutus">About Us</Link></li>
+              <li><Link to="/contactus">Contact Us</Link></li>
+              <li><Link to="/pagination">News</Link></li>
             </ul>
-        </nav>
+            {/* /NAV */}
+          </div>
+          {/* /responsive-nav */}
+        </div>
+        {/* /container */}
+      </nav>
     );
-}
-
-export default Nav;
+  }
