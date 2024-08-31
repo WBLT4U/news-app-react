009472091b6c32a75558745744f85f0326f2e90e
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2024-08-31 07:27:36 -0700

    f

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css'; // Ensure you have a CSS file for styles
 
 function Nav() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="navbar">
-            <ul className="navMenu">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/aboutus">About Us</Link></li>
-                <li><Link to="/contactus">Contact Us</Link></li>
-                <li><Link to="/pagination">News</Link></li>
+            <button className="menu-icon" onClick={toggleMenu}>
+                <i className="fas fa-bars"></i>
+            </button>
+            <ul className={`navMenu ${isOpen ? 'showMenu' : ''}`}>
+                <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+                <li><Link to="/aboutus" onClick={toggleMenu}>About Us</Link></li>
+                <li><Link to="/contactus" onClick={toggleMenu}>Contact Us</Link></li>
+                <li><Link to="/pagination" onClick={toggleMenu}>News</Link></li>
             </ul>
         </nav>
     );
