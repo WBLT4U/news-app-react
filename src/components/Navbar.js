301efc5dc961209ffbf9717c1f3bf74183a2aba4
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2024-09-01 02:57:30 -0700

    c

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,87 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import './navbar.css'; // We'll create this CSS file next
 
-const Nav = () => {
-  const [isMobile, setIsMobile] = useState(false);
+import { CodeIcon, HamburgetMenuClose, HamburgetMenuOpen } from "./Icons";
 
+function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
-    <nav className="navbar">
-      <h3 className="logo">Taofiq News App </h3>
-      <ul className={isMobile ? "nav-links-mobile" : "nav-links"}
-          onClick={() => setIsMobile(false)}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/aboutus">About Us</Link></li>
-        <li><Link to="/contactus">Contact Us</Link></li>
-        <li><Link to="/pagination">News</Link></li>
-      </ul>
-      <button className="mobile-menu-icon"
-              onClick={() => setIsMobile(!isMobile)}>
-        {isMobile ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i>}
-      </button>
-    </nav>
-  );
-};
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="/" className="nav-logo">
+            <span>Taofiq News React App</span>
+            {/* <i className="fas fa-code"></i> */}
+            
+          </NavLink>
 
-export default Nav;
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/aboutus"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/pagination"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                News
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/contactus"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            {/* <i className={click ? "fas fa-times" : "fas fa-bars"}></i> */}
+
+            {click ? (
+              <span className="icon">
+              <HamburgetMenuClose />
+            </span>
+              
+            ) : (
+              <span className="icon">
+                <HamburgetMenuOpen />{" "}
+              </span>
+              
+            )}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
